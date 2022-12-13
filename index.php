a396a21eecd224534e5e6bbf77b1fdaa29d6ec72
ihan.chen <ihan.chen@udnshopping.com>
2022-12-13 14:25:10 +0800

    feat: 新增標題 banner

--- index.php
+++ index.php
@@ -10,11 +10,22 @@ include_once("frame.php");
 <body>
   <div id="lsdv">
     <div class="container">
+      <div class="header">
+        <div class="header-title">
+          <img src="./images/title.svg" alt="" class="img-responsive header-title-img" />
+        </div>
+        <div class="header-bg">
+          <img src="./images/culturalMatsu_bg.svg" alt="" class="img-responsive header-bg-img" />
+        </div>
+      </div>
+
+      <h2 class="title">網站連結<small>ABOUT LINK</small></h2>
+
       <a class="col-md-4 col-xs-6 weblink" href="#">
         <div class="weblink-inner">
-          <img src="./images/memoryCulture.jpg" alt="" class="img-responsive">
+          <img src="./images/memoryCulture.jpg" alt="" class="img-responsive" />
           <div class="weblink-txt">
-            <h2 class="weblink-title">國家文化記憶庫</h2>
+            <h3 class="weblink-title">國家文化記憶庫</h3>
             <strong class="weblink-subtitle">memory.culture.tw</strong>
             <h3 class="weblink-cont">國家文化記憶庫是一個走入日常生活，尋找臺灣故事及原生文化的行動，讓我們一起挖掘地方，收集充滿溫度的故事，展示多采多姿的臺灣印象。</h3>
             <span class="weblink-more">Read more</span>
@@ -24,9 +35,9 @@ include_once("frame.php");
 
       <a class="col-md-4 col-xs-6 weblink" href="#">
         <div class="weblink-inner">
-          <img src="./images/memoryCulture.jpg" alt="" class="img-responsive">
+          <img src="./images/matsumemory.jpg" alt="" class="img-responsive" />
           <div class="weblink-txt">
-            <h2 class="weblink-title">國家文化記憶庫</h2>
+            <h3 class="weblink-title">國家文化記憶庫</h3>
             <strong class="weblink-subtitle">memory.culture.tw</strong>
             <h3 class="weblink-cont">國家文化記憶庫是一個走入日常生活，尋找臺灣故事及原生文化的行動，讓我們一起挖掘地方，收集充滿溫度的故事，展示多采多姿的臺灣印象。</h3>
             <span class="weblink-more">Read more</span>
@@ -36,9 +47,9 @@ include_once("frame.php");
 
       <a class="col-md-4 col-xs-6 weblink" href="#">
         <div class="weblink-inner">
-          <img src="./images/memoryCulture.jpg" alt="" class="img-responsive">
+          <img src="./images/matsugod.jpg" alt="" class="img-responsive" />
           <div class="weblink-txt">
-            <h2 class="weblink-title">國家文化記憶庫</h2>
+            <h3 class="weblink-title">國家文化記憶庫</h3>
             <strong class="weblink-subtitle">memory.culture.tw</strong>
             <h3 class="weblink-cont">馬祖好食是一個透過在地居民共同書寫，來紀錄馬祖飲食文化的線上平台。我們相信，文化是庶民生活經驗的總和，因此，只要是成長於這片土地上的人，都是最真實、最珍貴的在地知識擁有者。</h3>
             <span class="weblink-more">Read more</span>
@@ -48,9 +59,9 @@ include_once("frame.php");
 
       <a class="col-md-4 col-xs-6 weblink" href="#">
         <div class="weblink-inner">
-          <img src="./images/memoryCulture.jpg" alt="" class="img-responsive">
+          <img src="./images/matsufood.jpg" alt="" class="img-responsive" />
           <div class="weblink-txt">
-            <h2 class="weblink-title">國家文化記憶庫</h2>
+            <h3 class="weblink-title">國家文化記憶庫</h3>
             <strong class="weblink-subtitle">memory.culture.tw</strong>
             <h3 class="weblink-cont">國家文化記憶庫是一個走入日常生活，尋找臺灣故事及原生文化的行動，讓我們一起挖掘地方，收集充滿溫度的故事，展示多采多姿的臺灣印象。</h3>
             <span class="weblink-more">Read more</span>
