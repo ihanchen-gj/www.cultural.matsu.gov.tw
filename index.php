064a3ad76758f4aa942fe36900a17f7c83b6d0bb
ihan.chen <ihan.chen@udnshopping.com>
2022-12-13 14:37:46 +0800

    fix: 修改文字

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ include_once("frame.php");
 
       <h2 class="title">網站連結<small>ABOUT LINK</small></h2>
 
-      <a class="col-md-4 col-xs-6 weblink" href="#">
+      <a class="col-md-4 col-xs-6 weblink" href="https://memory.culture.tw/" target="_blank">
         <div class="weblink-inner">
           <img src="./images/memoryCulture.jpg" alt="" class="img-responsive" />
           <div class="weblink-txt">
@@ -33,37 +33,37 @@ include_once("frame.php");
         </div>
       </a>
 
-      <a class="col-md-4 col-xs-6 weblink" href="#">
+      <a class="col-md-4 col-xs-6 weblink" href="https://matsumemory.tw/" target="_blank">
         <div class="weblink-inner">
-          <img src="./images/matsumemory.jpg" alt="" class="img-responsive" />
+          <img src="./images/matsumemory.jpg" alt="馬祖文化記憶庫" class="img-responsive" />
           <div class="weblink-txt">
-            <h3 class="weblink-title">國家文化記憶庫</h3>
-            <strong class="weblink-subtitle">memory.culture.tw</strong>
-            <h3 class="weblink-cont">國家文化記憶庫是一個走入日常生活，尋找臺灣故事及原生文化的行動，讓我們一起挖掘地方，收集充滿溫度的故事，展示多采多姿的臺灣印象。</h3>
+            <h3 class="weblink-title">馬祖文化記憶庫</h3>
+            <strong class="weblink-subtitle">matsumemory.tw/</strong>
+            <h3 class="weblink-cont">本網站綜整馬祖文化記憶庫成果，為滾動式修正之參與平台，歡迎大家至「老照片」協助指認照片，若有物件及故事則可至「我要投稿」上傳，成果將於國家文化記憶庫呈現！</h3>
             <span class="weblink-more">Read more</span>
           </div>
         </div>
       </a>
 
-      <a class="col-md-4 col-xs-6 weblink" href="#">
+      <a class="col-md-4 col-xs-6 weblink" href="https://www.matsugod.net/" target="_blank">
         <div class="weblink-inner">
-          <img src="./images/matsugod.jpg" alt="" class="img-responsive" />
+          <img src="./images/matsugod.jpg" alt="馬祖好神" class="img-responsive" />
           <div class="weblink-txt">
-            <h3 class="weblink-title">國家文化記憶庫</h3>
-            <strong class="weblink-subtitle">memory.culture.tw</strong>
+            <h3 class="weblink-title">馬祖好神</h3>
+            <strong class="weblink-subtitle">www.matsugod.net</strong>
+            <h3 class="weblink-cont">馬祖好神彙整過往文化工作者調研成果，引領民眾了解馬祖信仰文化源流，認識神明故事、廟宇人文歷史和節慶，同時邀請民眾投稿，分享人與神的生活交流。。</h3>
+            <span class="weblink-more">Read more</span>
+          </div>
+        </div>
+      </a>
+
+      <a class="col-md-4 col-xs-6 weblink" href="https://www.matsufood.tw/" target="_blank">
+        <div class="weblink-inner">
+          <img src="./images/matsufood.jpg" alt="馬祖好食" class="img-responsive" />
+          <div class="weblink-txt">
+            <h3 class="weblink-title">馬祖好食</h3>
+            <strong class="weblink-subtitle">www.matsufood.tw</strong>
             <h3 class="weblink-cont">馬祖好食是一個透過在地居民共同書寫，來紀錄馬祖飲食文化的線上平台。我們相信，文化是庶民生活經驗的總和，因此，只要是成長於這片土地上的人，都是最真實、最珍貴的在地知識擁有者。</h3>
-            <span class="weblink-more">Read more</span>
-          </div>
-        </div>
-      </a>
-
-      <a class="col-md-4 col-xs-6 weblink" href="#">
-        <div class="weblink-inner">
-          <img src="./images/matsufood.jpg" alt="" class="img-responsive" />
-          <div class="weblink-txt">
-            <h3 class="weblink-title">國家文化記憶庫</h3>
-            <strong class="weblink-subtitle">memory.culture.tw</strong>
-            <h3 class="weblink-cont">國家文化記憶庫是一個走入日常生活，尋找臺灣故事及原生文化的行動，讓我們一起挖掘地方，收集充滿溫度的故事，展示多采多姿的臺灣印象。</h3>
             <span class="weblink-more">Read more</span>
           </div>
         </div>
